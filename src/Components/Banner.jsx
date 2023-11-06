e6dc1c8ef8d3c849b1f468954adfaecaf297b69a
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-11-07 01:32:40 +0600

    Meet Up sector added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className="carousel w-full h-[700px] rounded-2xl">
+    <div className="carousel w-full h-[700px] rounded-2xl my-4">
       <div id="slide1" className="carousel-item relative w-full">
         <img src="https://i.ibb.co/QXqpC92/R.jpg" className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
--- src/Layout/HomePage.jsx
+++ src/Layout/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Components/Banner';
 import ServiceFeature from '../Components/ServiceFeature';
+import MeetUp from '../Components/MeetUp';
 
 const HomePage = () => {
   return (
@@ -8,6 +9,8 @@ const HomePage = () => {
       <Banner/>
       <h1 className=" p-5 text-4xl font-bold border-l-8 text-yellow-600 rounded-l-2xl border-yellow-600 mt-8 md:mt-12 lg:mt-16 ">Special Service Features</h1>
       <ServiceFeature/>
+      <h1 className=" p-5 text-4xl font-bold text-right border-r-8 text-yellow-600 rounded-r-2xl border-yellow-600 mt-8 md:mt-12 lg:mt-16 ">Meet Up</h1>
+      <MeetUp/>
     </div>
   );
 };
